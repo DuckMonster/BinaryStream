0185eb93538b2bc25fa9ed59caa452eadf585433
Emil Ström <emil.strm@gmail.com>
2016-08-13 12:40:23 +0200

    Updated formatting

--- BinaryStream/BinaryStream/bstream.hpp
+++ BinaryStream/BinaryStream/bstream.hpp
@@ -9,7 +9,7 @@ public:
 
     // Write value to stream
     template<typename T>
-    void					write( const T& value );
+    void                    write( const T& value );
 
     // Read value from stream and advance cursor
     template<typename T>
@@ -17,31 +17,31 @@ public:
 
     // Read value from stream without advancing cursor
     template<typename T>
-    T						peek( );
+    T                       peek( );
 
     // Write operator
     template<typename T>
-    bstream&				operator<<( T& value );
+    bstream&                operator<<( T& value );
 
     // Read operator
     template<typename T>
-    bstream&				operator>>( T& output );
+    bstream&                operator>>( T& output );
 
     // Beginning of stream
-    const byte*				begin( ) const { return &buffer[0]; };
+    const byte*             begin( ) const { return &buffer[0]; };
     // Size of stream
-    unsigned int			size( ) const { return buffer.size( ); };
+    unsigned int            size( ) const { return buffer.size( ); };
 
 private:
-    std::vector<byte>		buffer;
-    size_t					cursor	= 0;
+    std::vector<byte>       buffer;
+    size_t                  cursor	= 0;
 };
 
 ///<summary>Write value into buffer</summary>
 template<typename T>
 inline void bstream::write( const T& value ) {
     // Create a pointer to memory
-    const void*	ptr		= &value;
+    const void* ptr = &value;
 
     // Go through this pointer and adding each byte value to buffer
     for (int i=0; i < sizeof( T ); i++) {
@@ -54,7 +54,7 @@ inline void bstream::write( const T& value ) {
 template<typename T>
 inline T bstream::read( ) {
     // Get value
-    T		value = peek<T>( );
+    T value = peek<T>( );
 
     // Advance cursor
     cursor += sizeof( T );
@@ -67,10 +67,10 @@ inline T bstream::read( ) {
 template<typename T>
 inline T bstream::peek( ) {
     // Pointer to memory
-    void*	ptr = &buffer[cursor];
+    void*	ptr     = &buffer[cursor];
 
     // Convert to T*
-    T*		tPtr = (T*)ptr;
+    T*		tPtr    = (T*)ptr;
 
     // Return value at adress
     return *tPtr;
