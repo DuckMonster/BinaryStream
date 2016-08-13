802f516d17157203d9e39d15cf57bcfd619f76ee
Emil Ström <emil.strm@gmail.com>
2016-08-13 11:50:55 +0200

    Writing and reading added for generic types

--- BinaryStream/BinaryStream/bstream.hpp
+++ BinaryStream/BinaryStream/bstream.hpp
@@ -1,10 +1,46 @@
 #pragma once
 #include <vector>
 
+typedef unsigned char byte;
+
 class bstream {
 public:
-	bstream( );
+	bstream( ) { };
+
+	template<typename T>
+	void					write( const T& value );
+
+	template<typename T>
+	T						read( );
 
 private:
-
+	std::vector<byte>		buffer;
+	size_t					cursor	= 0;
 };
+
+template<typename T>
+inline void bstream::write( const T& value ) {
+	// Create a pointer to memory
+	const void*	ptr		= &value;
+
+	// Go through this pointer and adding each byte value to buffer
+	for (int i=0; i < sizeof( T ); i++) {
+		// Convert void* to byte*, then tranverse the pointer i steps and get the value
+		buffer.push_back( *(((byte*)ptr) + i) );
+	}
+}
+
+template<typename T>
+inline T bstream::read( ) {
+	// Pointer to memory
+	void*	ptr = &buffer[cursor];
+
+	// Convert to T*
+	T*		tPtr = (T*)ptr;
+
+	// Advance cursor
+	cursor += sizeof( T );
+
+	// Return value at adress
+	return *tPtr;
+}
--- BinaryStream/BinaryStream/main.cpp
+++ BinaryStream/BinaryStream/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <bitset>
+#include "bstream.hpp"
+
 using namespace std;
 
+struct A {
+	int a = 50;
+	int b = 100;
+};
+
 int main( ) {
+	bstream str;
+
+	str.write<int>( 50 );
+	str.write<char[7]>( "Hello!" );
+	str.write( A( ) );
+
+	cout << str.read<int>( );
+	cout << ", ";
+	
+	for (int i=0; i < 7; i++)
+		cout << str.read<char>( );
+
+	A foo = str.read<A>( );
+
+	cout << ", " << foo.a << ":" << foo.b;
+
 	return 0;
 }
